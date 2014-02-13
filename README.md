7a19c2cf8446b4dad11c06fd83df807b4dcfa4ad
Ben Jones <ben.jones12@gmail.com>
2014-02-13 16:08:30 +1300

    Initial checkin

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-mqtt2xbmc
-=========
+# mqtt2xbmc
 
-Subscribe to MQTT topics (with wildcards) and post updates to XBMC
+This program subscribes to any number of MQTT topics (including wildcards) and publishes received payloads to [XBMC](http://xbmc.org/) as onscreen notification.
+
+You associate topic branches to XBMC hosts (<ipaddress>:<port>) in the configuration file (copy `mqtt2xbmc.conf.sample` to `mqtt2xbmc.conf` for use). 
+
+See details in the config sample for how to configure this script.
+
+## Requirements
+
+* An MQTT broker (e.g. [Mosquitto](http://mosquitto.org))
+* One or more XBMC hosts
+* The Paho Python module: `pip install paho-mqtt`
+
+## Installation
+
+mkdir /etc/mqtt2xbmc/
+git clone git://github.com/sumnerboy/mqtt2xbmc.git /usr/local/mqtt2xbmc/
+cp /usr/local/mqtt2xbmc/mqtt2xbmc.conf.sample /etc/mqtt2xbmc/mqtt2xbmc.conf
+cp /usr/local/mqtt2xbmc/mqtt2xbmc.init /etc/init.d/mqtt2xbmc
+update-rc.d mqtt2xbmc defaults
+cp /usr/local/mqtt2xbmc/mqtt2xbmc.default /etc/default/mqtt2xbmc
+
+* Edit /etc/default/mqtt2xbmc and /etc/mqtt2xbmc/mqtt2xbmc.conf to suit
+
+/etc/init.d/mqtt2xbmc start
---
+++ mqtt2xbmc.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+import urllib2
+import paho.mqtt.client as paho   # pip install paho-mqtt
+import logging
+import signal
+import sys
+import time
+
+__author__    = 'Ben Jones <ben.jones12()gmail.com>'
+__copyright__ = 'Copyright 2014 Ben Jones'
+__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+
+# load configuration
+conf = {}
+try:
+    execfile('mqtt2xbmc.conf', conf)
+except Exception, e:
+    print "Cannot load mqtt2xbmc.conf: %s" % str(e)
+    sys.exit(2)
+
+LOGFILE = conf['logfile']
+LOGLEVEL = conf['loglevel']
+LOGFORMAT = conf['logformat']
+
+MQTT_HOST = conf['broker']
+MQTT_PORT = int(conf['port'])
+MQTT_LWT = conf['lwt']
+
+# initialise logging    
+logging.basicConfig(filename=LOGFILE, level=LOGLEVEL, format=LOGFORMAT)
+logging.info("Starting mqtt2xbmc")
+logging.info("INFO MODE")
+logging.debug("DEBUG MODE")
+
+# initialise MQTT broker connection
+mqttc = paho.Client('mqtt2xbmc', clean_session=False)
+
+# check for authentication
+if conf['username'] is not None:
+    mqttc.username_pw_set(conf['username'], conf['password'])
+
+# configure the last-will-and-testament
+mqttc.will_set(MQTT_LWT, payload="mqtt2xbmc", qos=0, retain=False)
+
+def connect():
+    """
+    Connect to the broker
+    """
+    logging.debug("Attempting connection to MQTT broker %s:%d..." % (MQTT_HOST, MQTT_PORT))
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+    mqttc.on_disconnect = on_disconnect
+
+    result = mqttc.connect(MQTT_HOST, MQTT_PORT, 60)
+    if result == 0:
+        mqttc.loop_forever()
+    else:
+        logging.info("Connection failed with error code %s. Retrying in 10s...", result)
+        time.sleep(10)
+        connect()
+         
+def disconnect(signum, frame):
+    """
+    Signal handler to ensure we disconnect cleanly 
+    in the event of a SIGTERM or SIGINT.
+    """
+    logging.debug("Disconnecting from MQTT broker...")
+    mqttc.loop_stop()
+    mqttc.disconnect()
+    logging.debug("Exiting on signal %d", signum)
+    sys.exit(signum)
+
+def notify_xbmc(xbmchost, title, message):
+    command = '{"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"%s","message":"%s"},"id":1}' % (title, message)
+    command = command.encode('utf-8')
+    url = 'http://%s/jsonrpc' % (xbmchost)
+    try:
+        req = urllib2.Request(url, command)
+        req.add_header("Content-type", "application/json")
+        response = urllib2.urlopen(req)
+    except urllib2.URLError, e:
+        logging.error("URLError sending %s to %s: %s" % (url, xbmchost, str(e)))
+    except Exception, e:
+        logging.error("Error sending JSON request to %s: %s" % (xbmchost, str(e)))
+  
+def on_connect(mosq, userdata, result_code):
+    logging.debug("Connected to MQTT broker, subscribing to topics...")
+    for topic in conf['topichost'].keys():
+        logging.debug("Subscribing to %s" % topic)
+        mqttc.subscribe(topic, 0)
+
+def on_message(mosq, userdata, msg):
+    """
+    Message received from the broker
+    """
+    topic = msg.topic
+    payload = str(msg.payload)
+    logging.debug("Message received on %s: %s" % (topic, payload))
+
+    hosts = conf['topichost'][topic]
+    title = conf['topictitle'][topic]
+
+    for host in hosts:
+        logging.debug("Sending notification to %s..." % host)
+        xbmchost = conf['xbmchost'][host]
+        notify_xbmc(xbmchost, title, payload)
+
+def on_disconnect(mosq, userdata, result_code):
+    """
+    Handle disconnections from the broker
+    """
+    if result_code == 0:
+        logging.info("Clean disconnection")
+    else:
+        logging.info("Unexpected disconnection! Reconnecting in 5 seconds...")
+        logging.debug("Result code: %s", result_code)
+        time.sleep(5)
+        connect()
+
+# use the signal module to handle signals
+signal.signal(signal.SIGTERM, disconnect)
+signal.signal(signal.SIGINT, disconnect)
+        
+# connect to broker and start listening
+connect()
